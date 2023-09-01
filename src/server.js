df5a938430420e118522c3723bac7cf6b7cfa838
warmachine028 <warmachine028@outlook.com>
2023-09-01 23:30:33 +0530

    add: initial commit

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ import express from "express"
 import { graphqlHTTP as expressGraphQL } from "express-graphql"
 import { GraphQLSchema } from 'graphql'
 import { RootQueryType, RootMutationType } from "./models/index.js"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
 const schema = new GraphQLSchema({
     query: RootQueryType,
@@ -12,4 +16,4 @@ const schema = new GraphQLSchema({
 
 
 app.use('/graphql', expressGraphQL({ schema, graphiql: true }))
-app.listen(5000, () => console.log("Server Running"))
+app.listen(PORT, () => console.log("Server Running"))
