3586df279c962e22f783679d540835dff659312a
warmachine028 <warmachine028@outlook.com>
2023-09-04 13:50:49 +0530

    chores: fixes for deployment

--- src/server.js
+++ src/server.js
@@ -8,12 +8,12 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
-
+console.log(process.env.PORT, PORT)
 const schema = new GraphQLSchema({
     query: RootQueryType,
     mutation: RootMutationType
 })
 
-app.get('/', (_, res) => res.send('Hello to Learning GraphQL'))
+app.get('/', (_, res) => res.send('<h2 style="margin:10;font-family:roboto,sans-serif;font-weight:normal"> Hello to Learning GraphQL</h2>'))
 app.use('/graphql', expressGraphQL({ schema, graphiql: true }))
 app.listen(PORT, () => console.log("Server Running"))
