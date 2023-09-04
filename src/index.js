504bddbd7238929935f5d310bd78d141d44f5102
warmachine028 <warmachine028@outlook.com>
2023-09-04 15:00:42 +0530

    chores: fixes for deployment

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ import { graphqlHTTP as expressGraphQL } from "express-graphql"
 import { GraphQLSchema } from 'graphql'
 import { RootQueryType, RootMutationType } from "./models/index.js"
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
-const PORT = 5000
-
+const PORT = process.env.PORT || 5000
 const schema = new GraphQLSchema({
     query: RootQueryType,
     mutation: RootMutationType
 })
 
-app.get('/', (_, res) => res.send('<h2 style="margin:10;font-family:roboto,sans-serif;font-weight:normal"> Hello to Learning GraphQL</h2>'))
+app.use(cors())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use('/graphql', expressGraphQL({ schema, graphiql: true }))
+
+app.get('/', (_, res) => res.send('<h2 style="margin:10;font-family:roboto,sans-serif;font-weight:normal"> Hello to Learning GraphQL</h2>'))
 app.listen(PORT, () => console.log("Server Running"))
