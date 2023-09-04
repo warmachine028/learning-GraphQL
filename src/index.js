31b77cbf505fc3a9355e7641a069fd95a32aded8
warmachine028 <warmachine028@outlook.com>
2023-09-04 15:24:06 +0530

    chores: fixes for deployment

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { GraphQLSchema } from 'graphql'
 import { RootQueryType, RootMutationType } from "./models/index.js"
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const schema = new GraphQLSchema({
     query: RootQueryType,
     mutation: RootMutationType
