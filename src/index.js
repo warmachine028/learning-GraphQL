ec1648e4c7baea2652533b0485318a2a12c10ad8
warmachine028 <warmachine028@outlook.com>
2023-09-04 22:25:23 +0530

    chores: fixes for deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { graphqlHTTP as expressGraphQL } from "express-graphql"
 import express from "express"
 import { GraphQLSchema } from 'graphql'
 import { RootQueryType, RootMutationType } from "./models/index.js"
+import { createHandler } from "graphql-http/lib/use/express"
 
 const app = express()
 const PORT = 5000
@@ -10,7 +10,6 @@ const schema = new GraphQLSchema({
     mutation: RootMutationType
 })
 
-app.use('/graphql', expressGraphQL({ schema, graphiql: true }))
-
-app.get('/', (_, res) => res.send('<h2 style="margin:10;font-family:roboto,sans-serif;font-weight:normal"> Hello to Learning GraphQL</h2>'))
+app.use('/graphql', createHandler({ schema, graphiql: true }))
+app.get('/', (_, res) => res.send("<h2 style='margin:10; font-family:roboto,sans-serif; font-weight:normal;'>Hello to Learning GraphQL</h2>"))
 app.listen(PORT, () => console.log("Server Running"))
